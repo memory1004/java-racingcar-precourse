afd692768110e485671cedbe54dc261f9167b0ee
hue <huttchang@gmail.com>
2022-04-25 23:11:41 +0900

    refactor: 검증로직 리팩토링

--- src/main/java/racingcar/CommandLineInterface.java
+++ src/main/java/racingcar/CommandLineInterface.java
@@ -10,9 +10,13 @@ public class CommandLineInterface {
     private int round;
     private List<String> separatedCarnames;
 
-    private CommandLineInterface(){
+    private static final String INPUT_CARNAME_MESSAGE = "경주 할 자동차이름(이름은쉼표(,)기준으로구분)";
+    private static final String INPUT_ROUND_MESSAGE = "시도할 회수는 몇회인가요?";
+    private static final String INVALID_CARNAME_LENGTH = "자동차이름은 5글자이하로 작성해주세요.";
+    private static final String INVALID_DUPLICATED_CARNAME = "자동차이름은 5글자이하로 작성해주세요.";
+    private static final String INVALID_ROUND_TEXT = "숫자만 입력해주세요.";
 
-    }
+    private CommandLineInterface(){}
 
     public static CommandLineInterface getInstance(){
         return new CommandLineInterface();
@@ -20,24 +24,29 @@ public class CommandLineInterface {
 
     public void inputCarNames() {
         this.separatedCarnames = new ArrayList<>();
+        System.out.println("call!!!!!!!!!!!");
         try {
-            System.out.println("경주 할 자동차이름(이름은쉼표(,)기준으로구분)");
-            for (String carname : inputText().split(",")) {
+            System.out.println(INPUT_CARNAME_MESSAGE);
+            String inputText = inputText();
+            for (String carname : inputText.split(",")) {
                 validationCarname(carname);
                 separatedCarnames.add(carname);
             }
+            System.out.println(String.join(",", inputText));
         } catch (IllegalArgumentException e) {
-            System.out.println("[ERROR] ");
+            e.printStackTrace();
+            System.out.println(String.format("[ERROR] : %s", e.getMessage()));
             inputCarNames();
+
         }
     }
 
     private void validationCarname(String carname) {
         if (carname.length() > 5) {
-            throw new IllegalArgumentException("자동차이름은 5글자이하로 작성해주세요.");
+            throw new IllegalArgumentException(INVALID_CARNAME_LENGTH);
         }
         if ( this.separatedCarnames.contains(carname)){
-            throw new IllegalArgumentException("같은 자동차이름은 등록이 불가능합니다.");
+            throw new IllegalArgumentException(INVALID_DUPLICATED_CARNAME);
         }
     }
 
@@ -51,10 +60,11 @@ public class CommandLineInterface {
 
     public void inputRound() {
         try {
-            System.out.println("시도할 회수는 몇회인가요?");
+            System.out.println(INPUT_ROUND_MESSAGE);
             round = Integer.parseInt(inputText());
+            System.out.println(round);
         } catch (NumberFormatException numberFormatException) {
-            System.out.println("[ERROR] : 숫자만 입력해주세요.");
+            System.out.println(String.format("[ERROR] : %s", INVALID_ROUND_TEXT));
             inputRound();
         }
     }
