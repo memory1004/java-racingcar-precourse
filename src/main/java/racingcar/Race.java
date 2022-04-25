771d60188f6a6ae4e1f5fa08203129da6f922200
hue <huttchang@gmail.com>
2022-04-25 23:12:26 +0900

    feat: 가장큰 거리구하는 메서드 및 최종 우승자를 가리는 함수 추가

--- src/main/java/racingcar/Race.java
+++ src/main/java/racingcar/Race.java
@@ -6,6 +6,7 @@ import java.util.List;
 public class Race {
     private List<RacingCar> racingCars;
     private int round;
+
     private Race() {
 
     }
@@ -28,14 +29,40 @@ public class Race {
     }
 
     public void start(){
+        System.out.println("실행 결과");
         for (int i = 0; i < this.round; i++) {
-            this.getRacingCars()
-                    .forEach(RacingCar::start);
-            System.out.println("=========");
+            this.getRacingCars().forEach(RacingCar::start);
+            System.out.println("");
         }
     }
 
     private List<RacingCar> getRacingCars() {
         return this.racingCars;
     }
+
+    public void printResult(){
+        String printFormat = "최종 우승자는 %s 입니다.";
+        System.out.println(String.format(printFormat, String.join(", ", getWinners())));
+    }
+
+    private List<String> getWinners(){
+        List<String> winners = new ArrayList<>();
+        for (RacingCar racingCar : getRacingCars()) {
+            if ( maxDistance() <= racingCar.getTotalDistance()){
+                winners.add(racingCar.getName());
+            }
+        }
+        return winners;
+    }
+
+    private int maxDistance(){
+        int maxDistance = 0;
+        for (RacingCar racingCar : getRacingCars()) {
+            if (maxDistance < racingCar.getTotalDistance()){
+                maxDistance = racingCar.getTotalDistance();
+            }
+        }
+        return maxDistance;
+    }
+
 }
